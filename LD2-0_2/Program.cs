8a1af8fc6d9bd8b0a3740c32f4dce348828ef565
Tomas Odinas <tomas.odinas@ktu.edu>
2020-04-25 21:06:33 +0300

    2 padarytas

---
+++ LD2-0_2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace LD2_0_2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[,] a =
+            {
+                {1, 3, 3},
+                {2, 3, 3},
+                {1, 2, 4},
+            };
+
+            Console.WriteLine("K = " + MaxEqualRectangle(a));
+        }
+
+        static int MaxEqualRectangle(int[,] arr)
+        {
+            int rows = arr.GetLength(0);
+            int cols = arr.GetLength(1);
+
+            int[,] ats = new int [rows, cols];
+            int result = 0;
+
+            for (int row = 0; row < rows; row++)
+            for (int col = 0; col < cols; col++)
+            {
+                // pirmojo stulpelio ar eilutes elementai kvadrato nesudarys, tai jie lieka kaip 1x1 
+                if (row == 0 || col == 0) ats[row, col] = 1;
+
+                else
+                {
+                    // jei kvadrata sudaro 4 vienodi elementai, tada ats = Ats minimumas + 1 
+                    if (arr[row, col] == arr[row - 1, col] &&
+                        arr[row, col] == arr[row, col - 1] &&
+                        arr[row, col] == arr[row - 1, col - 1])
+
+                        ats[row, col] = Math.Min(
+                                            ats[row - 1, col], Math.Min(
+                                                ats[row, col - 1],
+                                                ats[row - 1, col - 1])
+                                        ) + 1;
+
+                    // jei elementai ne vienodi, tai bazinis atvejis
+                    else ats[row, col] = 1;
+                }
+
+                // isimenam didziausia ats reiksme
+                result = Math.Max(result, ats[row, col]);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LD2-0_3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace LD2_0_3
+{
+    internal class Program
+    {
+        private static int[] s;
+        private static int[] p;
+
+        private static int[,] cache;
+
+        public static void Main(string[] args)
+        {
+            //  int max = int.Parse(args[0]);
+
+            int max = 2000;
+
+            var rand = new Random();
+            s = GetRandomArray(500, max);
+            p = GetRandomArray(500, max);
+            int n = s.Length;
+            int w = (int) s.Average();
+            Stopwatch st = Stopwatch.StartNew();
+
+            Console.WriteLine("Metodas;Dydis;Reiksme;Laikas");
+            for (int i = 1; i <= 300; i++)
+            {
+                Console.Write($"Ga;{i};{Ga(i, w)};");
+                st.Stop();
+                Console.WriteLine($"{st.Elapsed}");
+                st.Reset();
+            }
+        }
+
+
+        static int Ga(int k, int r)
+        {
+            if (r == 0 || k == 0) return 0;
+
+            if (s[k - 1] > r) return Ga(k - 1, r);
+
+            else return Math.Max(Ga(k - 1, r), p[k - 1] + Ga(k - 1, r - s[k - 1]));
+        }
+
+        private static int[] GetRandomArray(int length, int max)
+        {
+            var arr = new int[length];
+            var rand = new Random();
+
+            for (int i = 0; i < length; i++)
+                arr[i] = rand.Next(max);
+
+            return arr;
+        }
+    }
+}
